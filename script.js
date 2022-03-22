94d26662283e9aa6cd4a93270cc3b11ddbfc15fd
DeepaThangaraju <91173451+DeepaThangaraju@users.noreply.github.com>
2022-03-22 14:35:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var typed=new Typed(".typing",{
+    strings:[" ","Full Stack Developer","Web Designer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+
+})
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
